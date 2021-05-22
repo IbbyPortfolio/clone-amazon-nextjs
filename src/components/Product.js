c5927254e5962e62414d2445a6ed3a63e67827ad
Ibrahim <ibrahim.s.amin@gmail.com>
2021-05-23 00:03:48 +1000

    main page completed

--- src/components/Product.js
+++ src/components/Product.js
@@ -34,7 +34,7 @@ const Product = ({ product }) => {
          </div>
 
          {hasPrime && (
-            <div className='flex items-center space-x-2 mt-5'>
+            <div className='flex items-center space-x-2 -mt-5'>
                <img
                   className='w-12'
                   src='https://links.papareact.com/fdw'
